d129ed3a76802edc4ee327e0e2108cd4475ee6a9
soumaya <soumiatinghir119@gmail.com>
2023-12-04 10:25:17 +0100

    adding authentification and login

---
+++ auth/src/html/pages/auths/connect.php
@@ -0,0 +1,12 @@
+<?php
+$host="localhost";
+$user="root";
+$pw="";
+$dbname="quizex";
+$con=mysqli_connect($host,$user,$pw,$dbname);
+// if($con){
+// echo"connected";
+// }else{
+//     echo"error";
+// }
+?>
