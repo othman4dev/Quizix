a815e6401ccf5bf8118876d0f04e8655be81c4aa
soumaya <soumiatinghir119@gmail.com>
2023-12-08 09:41:14 +0100

    Adding login and logoout functionalities

--- Admin/src/html/lms/logout.php
+++ Admin/src/html/lms/logout.php
@@ -3,6 +3,6 @@ session_start();
 session_unset();
 session_destroy();
 echo"hello";
-header('location:Quizix/auth/src/html/pages/auths/auth-login.php')
+header('location:Quizix/auth/src/html/pages/auths/auth-login.php');
 
 ?>
